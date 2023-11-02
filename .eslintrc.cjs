45ef65d23b238da7f0805de9ff5cf413a764cbb4
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2023-11-03 05:33:59 +0700

    enforce stricter eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,8 +10,11 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    // 'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:vitest/recommended',
     'plugin:storybook/recommended',
@@ -32,7 +35,8 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
   },
   plugins: [
     '@typescript-eslint',
@@ -42,6 +46,8 @@ module.exports = {
     'prettier',
   ],
   rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
     'react/react-in-jsx-scope': 'off',
     'react-refresh/only-export-components': [
       'warn',
