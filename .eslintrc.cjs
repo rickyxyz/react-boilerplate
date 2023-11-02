d4241f18435196186c32145e620f75bb68af4c3b
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2023-11-03 02:20:37 +0700

    add storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,18 @@ module.exports = {
     browser: true,
     es2021: true,
   },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:vitest/recommended',
+    'plugin:storybook/recommended',
     'plugin:prettier/recommended',
   ],
   overrides: [
