069a4b5e1aa4c58091a038bf5a7a626a8fd5a19f
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2023-12-10 13:44:20 +0700

    feat: add style template

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import './global.css';
 
 ReactDOM.createRoot(document.getElementById('root') as Element).render(
   <React.StrictMode>
