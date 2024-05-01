75578b131830daa593c530da9e1a1c67b0779016
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2024-05-01 16:51:40 +0700

    feat: update storybook version

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,49 +1,21 @@
 import type { StorybookConfig } from '@storybook/react-vite';
-import path from 'path';
 
 const config: StorybookConfig = {
-  stories: [
-    '../stories/**/*.mdx',
-    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-  ],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
+    '@storybook/addon-onboarding',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
     '@storybook/addon-interactions',
-    'storybook-dark-mode',
   ],
-  core: {
-    builder: '@storybook/builder-vite',
-  },
+  staticDirs: ['../public'],
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
   docs: {
     autodocs: 'tag',
-  },
-  // eslint-disable-next-line @typescript-eslint/require-await
-  async viteFinal(config) {
-    return {
-      ...config,
-      resolve: {
-        alias: [
-          {
-            find: '@',
-            replacement: path.resolve(__dirname, './src'),
-          },
-          {
-            find: '@assets',
-            replacement: path.resolve(__dirname, './src/assets'),
-          },
-          {
-            find: '@components',
-            replacement: path.resolve(__dirname, './src/components'),
-          },
-        ],
-      },
-    };
   },
 };
 export default config;
